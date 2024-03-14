7dc803911ef0d32598cd071f3ffa2e1626de7b8a
iamhojiakbar <farruxaktamov@gmail.com>
2024-03-14 09:53:31 +0300

    I added new buttons

--- buttons.py
+++ buttons.py
@@ -1,1 +1,9 @@
-from aiogram.types import ReplyKeyboardMarkup,
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+
+keyword = ReplyKeyboardMarkup(
+    keyboard=[
+
+        [KeyboardButton(text="Button 1"), KeyboardButton(text="Button 2")],
+        [KeyboardButton(text="Button 3"), KeyboardButton(text="Button")],
+    ], resize_keyboard=True
+)
